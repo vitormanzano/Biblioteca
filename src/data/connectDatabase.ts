696a316bbd6bb5e1a8bfc26a53831297a1038e51
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2024-12-19 21:09:50 -0300

    Conectando ao banco e fazendo a função de listar todos os livros por meio do banco de dados Oracle SQL

---
+++ src/data/connectDatabase.ts
@@ -0,0 +1,18 @@
+import oracledb from 'oracledb';
+
+const dbConfig = {
+    user: process.env.ORACLE_USER,
+    password: process.env.ORACLE_PASSWORD,
+    connectString: process.env.ORACLE_CONN_STR
+};
+
+export async function conectarBanco() {
+    try{
+        let connection = await oracledb.getConnection(dbConfig);
+        return connection;
+    }
+    catch (err) {
+        console.error('Erro ao conectar: ',err);
+        return undefined
+    }
+}
