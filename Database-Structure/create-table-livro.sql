6cdb10fc4083d00f0c8e1826dd24712921edb295
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-07 16:50:56 -0300

    Normalizando autor

--- Database-Structure/create-table-livro.sql
+++ Database-Structure/create-table-livro.sql
@@ -1,6 +1,7 @@
 CREATE TABLE LIVRO (
     GUID CHAR(36) PRIMARY KEY,
     titulo VARCHAR2(100) NOT NULL,
-    autor VARCHAR2(100),
-    paginas INTEGER
+    autor_guid VARCHAR2(100),
+    paginas INTEGER,
+    FOREIGN KEY (autor_guid) REFERENCES autor(guid)
 );
