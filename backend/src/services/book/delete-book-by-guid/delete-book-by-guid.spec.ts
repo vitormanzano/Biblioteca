81dd3cbe7a17bdc43bbf1e7876c2df38863ec719
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-30 12:22:55 -0300

    delete book test

---
+++ backend/src/services/book/delete-book-by-guid/delete-book-by-guid.spec.ts
@@ -0,0 +1,31 @@
+import { IBooksRepository } from "backend/src/repositories/models-repository/book-repository-interface";
+import { DeleteBookByGuidService } from "./delete-book-by-guid-service";
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryBookRepository } from "backend/src/repositories/in-memory/in-memory-book";
+import { BookModel } from "backend/src/models/book-model";
+
+let booksRepository: IBooksRepository;
+let sut: DeleteBookByGuidService;
+
+describe('Delete book by guid service', async () => {
+    beforeEach(() => {
+        booksRepository = new InMemoryBookRepository();
+        sut = new DeleteBookByGuidService(booksRepository);
+    });
+
+    it('Should be able to delete a book by guid', async () => {
+        const bookForCreation: BookModel = {
+            GUID: '1234',
+            titulo: 'livroTeste',
+            autor: 'autorTeste',
+            paginas: 10
+        }; 
+
+        
+        await booksRepository.insertBook(bookForCreation);
+
+        const hasDeletedBook = await booksRepository.deleteBookByGuid('1234');
+
+        expect(hasDeletedBook).toEqual(true);
+    });
+})
