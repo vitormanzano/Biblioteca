847b5fef4558841d020135f0b1c0964650320cbb
Vitor Manzano Villela Dias <vimanzanovillela@gmail.com>
2025-02-02 23:01:41 -0300

    Project with find by email

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,27 +1,30 @@
 import {Router} from "express";
-import * as BookController from "./controllers/book-controller";
+// import * as BookController from "./controllers/book-controller";
 import * as UserController from "./controllers/user-controller";
 
 const router = Router();
 
 //Rotas de livros
 
-router.get("/books", BookController.getAllBooks);
-router.get("/books/:guid", BookController.getBookByGuid);
-router.get("/books/search/:title", BookController.getBooksByTitle);
+// router.get("/books", BookController.getAllBooks);
+// router.get("/books/:guid", BookController.getBookByGuid);
+// router.get("/books/search/:title", BookController.getBooksByTitle);
 
-router.post("/books", BookController.postBook);
+// router.post("/books", BookController.postBook);
 
-router.patch("/books/:guid", BookController.updateBookByGuid);
+// router.patch("/books/:guid", BookController.updateBookByGuid);
 
-router.delete("/books/:guid", BookController.deleteBookByGuid);
+// router.delete("/books/:guid", BookController.deleteBookByGuid);
 
 //Rotas do usuário
 
-router.get("/user/:cpf", UserController.getUserByCpf);
+const userBasePath = "/user"
+
+router.get(`${userBasePath}/:cpf`, UserController.getUserByCpf);
 router.get("/user", UserController.getAllUsers);
 
 router.post("/user", UserController.postUser);
+router.post("/user/signIn", UserController.signInUser);
 
 router.delete("/user/:cpf", UserController.deleteUserByCpf);
 
